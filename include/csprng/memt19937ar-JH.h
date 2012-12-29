ce88789ad253c833faec27e71e081e109905e522
jirka <jirka@zrout3.(none)>
2012-12-29 15:33:25 +0100

    Updated headers

--- include/csprng/memt19937ar-JH.h
+++ include/csprng/memt19937ar-JH.h
@@ -1,48 +1,28 @@
 /* 
-   Adapted by Jiri Hladky, hladky DOT jiri AT gmail DOT com
+This is a maximally equidistributed Mersenne Twister MEMT19937 by Shin Harase (2010/1/25).
 
-   This is a maximally equidistributed Mersenne Twister MEMT19937 by Shin Harase (2010/1/25).
+Copyright (C) 1997 - 2002, Makoto Matsumoto and Takuji Nishimura
+Copyright (C) 2011, 2012 Jirka Hladky
 
-   A C-program for MT19937, with initialization improved 2002/1/26.
-   Coded by Takuji Nishimura and Makoto Matsumoto.
+This file is part of CSRNG http://code.google.com/p/csrng/
 
-   Before using, initialize the state by using init_genrand(seed)  
-   or init_by_array(init_key, key_length).
+CSRNG is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-   Copyright (C) 1997 - 2002, Makoto Matsumoto and Takuji Nishimura,
-   All rights reserved.                          
+CSRNG is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-   Redistribution and use in source and binary forms, with or without
-   modification, are permitted provided that the following conditions
-   are met:
+You should have received a copy of the GNU General Public License
+along with CSRNG.  If not, see <http://www.gnu.org/licenses/>.
 
-     1. Redistributions of source code must retain the above copyright
-        notice, this list of conditions and the following disclaimer.
-
-     2. Redistributions in binary form must reproduce the above copyright
-        notice, this list of conditions and the following disclaimer in the
-        documentation and/or other materials provided with the distribution.
-
-     3. The names of its contributors may not be used to endorse or promote 
-        products derived from this software without specific prior written 
-        permission.
-
-   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-   A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-
-   Any feedback is very welcome.
-   http://www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/emt.html
-   email: m-mat @ math.sci.hiroshima-u.ac.jp (remove space)
+Any feedback is very welcome.
+http://www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/emt.html
+email: m-mat @ math.sci.hiroshima-u.ac.jp (remove space)
+email: hladky DOT jiri AT gmail DOT com
 */
 
 #ifndef MEMT_H
--- src/QRBG.h
+++ src/QRBG.h
@@ -1,40 +1,25 @@
-/******************************************************************************
-*
-* Declaration of the QRBG Service Access class. (For definition see: QRBG.cpp 
-* file)
-*
-* Designed and written by Radomir Stevanovic, Jan/2007.
-* Developed in Rudjer Boskovic Institute, Zagreb, Croatia.
-* Contact: stevie@ieee.org.
-*
-* Last revision: 2007-07-19
-* Version: 0.31
-*
-* NOTE: QRBG code compiles on both Linux and Windows machines (tested with 
-*       gcc/g++ 3.2.2 and Microsoft Visual C++ 8.0)
-*
-*****************************************************************************
-*
-* Copyright (c) 2007 Radomir Stevanovic and Rudjer Boskovic Institute.
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-* THE SOFTWARE.
-*
+/*
+QRBG Service API - see http://random.irb.hr/
+To retrieve the random data from this service you will need to 
+go to the http://random.irb.hr/ and register there first
+
+Copyright (C) 2007 Radomir Stevanovic and Rudjer Boskovic Institute.
+Copyright (C) 2011, 2012 Jirka Hladky
+
+This file is part of CSRNG http://code.google.com/p/csrng/
+
+CSRNG is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+CSRNG is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with CSRNG.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #pragma once
